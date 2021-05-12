bdb67d8a8dd78f97ce4aebc57343b0b6cea7a141
ball200722540 <ball20072540@gmail.com>
2021-05-13 01:03:02 +0700

    add pk v5

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
-const Sequelize = require('sequelize');
-const env = require('../env');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-    host: env.host,
-    dialect: env.dialect,
-    operatorsAliases: false,
-    pool: {
-      max: env.max,
-      min: env.pool.min,
-      acquire: env.pool.acquire,
-      idle: env.pool.idle
-    }
-});
+// const Sequelize = require('sequelize');
+// const env = require('../env');
+// const sequelize = new Sequelize(env.database, env.username, env.password, {
+//     host: env.host,
+//     dialect: env.dialect,
+//     operatorsAliases: false,
+//     pool: {
+//       max: env.max,
+//       min: env.pool.min,
+//       acquire: env.pool.acquire,
+//       idle: env.pool.idle
+//     }
+// });
 
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// const db = {};
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
 //---------------------------------- Table ---------------------------------- 
 // db.User = require('../models/userModel/user')(sequelize, Sequelize);
