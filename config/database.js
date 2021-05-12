2d6c6e4428e6f48bd335686ec8b015577d3fcdd6
ball200722540 <ball20072540@gmail.com>
2021-05-13 02:42:17 +0700

    add pk v41

--- config/database.js
+++ config/database.js
@@ -36,7 +36,7 @@ db.BusinessLogo = require('../models/businessModel/businessLogo')(sequelize, Seq
 db.BusinessImage = require('../models/businessModel/businessImage')(sequelize, Sequelize);
 
 db.DeliveryService = require('../models/deliveryModel/deliveryService')(sequelize, Sequelize);
-db.Packaging = require('../models/packagingModel/packaging')(sequelize, Sequelize);
+// db.Packaging = require('../models/packagingModel/packaging')(sequelize, Sequelize);
 
 // db.ProductSuffix = require('../models/productModel/productSuffix')(sequelize, Sequelize);
 // db.ProductType = require('../models/productModel/productType')(sequelize, Sequelize);
